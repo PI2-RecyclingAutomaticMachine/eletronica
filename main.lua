5da1ea9d520290264c0c5d0b205221479a0a07ab
matheusjerico <matheusjerico1994@hotmail.com>
2018-05-03 15:36:22 -0300

    atualização do main.

--- main.lua
+++ main.lua
@@ -1,7 +1,3 @@
-wlan = dofile('wlan.lua')
-mqtt = dofile('mqtt.lua')
-motor = dofile('motor.lua')
-
 MODOS_OPERACAO = {NORMAL = 'normal', ATUALIZACAO = 'atualizacao'}
 
 function loop_principal(timer_principal)
@@ -19,80 +15,38 @@ function loop_principal(timer_principal)
   end)
 
   if (modo_operacao == MODOS_OPERACAO.NORMAL) then
-
+ 
+	  gpio.mode(2, gpio.INPUT) -- SENSOR PROXIMO AO TRITURADOR
+	  gpio.mode(3, gpio.INPUT) -- SENSOR MAIS DISTANTE DO TRITURADOR
+	  gpio.mode(1, gpio.OUTPUT) -- RELE PARA ACIONAR TRITURADOR
+		gpio.mode(4, gpio.OUTPUT) -- RELE PARA ACIONAR O FREIO
+    
     print('Entrando no loop principal...');
-    timer_principal:register(7000, tmr.ALARM_AUTO, function()
-      wlan.status_wifi();
+    timer_principal:register(2500, tmr.ALARM_AUTO, function()
 
-      topico_receber = "#"
-      topico_base_publicacao = "raspberry"
-      cliente_mqtt.mqtt_dados(topico_base_publicacao, topico_receber, dados);
-      topico = cliente_mqtt.mqtt_topico
-      conteudo = cliente_mqtt.mqtt_arquivo
-      peso = tonumber(conteudo)
+	  	if ( gpio.read(2) == 0 and gpio.read(3) == 0 ) then
+	  		print("Funil do triturador está vazio.")	
+	  		tmr.delay(1000000)
+	  		if ( gpio.read(2) == 0 and gpio.read(3) == 0 ) then
+		  		gpio.write(1, gpio.LOW)
+		  		gpio.write(4, gpio.HIGH)
+		  		tmr.delay(5000000) -- ENERGIA CONTROLA
+		  		gpio.write(4, gpio.LOW)
 
+		  	else
+		  		print("Movimento de garrafas no triturador.")
+		  	end
 
-      sensor_capacitivo_vidro = gpio.read(3)
-      sensor_capacitivo_plastico = gpio.read(4)
-      sensor_capacitivo_verificar = gpio.read(5)
-      --hx711.init(clk, data)
-      hx711.init(6, 1)
-      read_hx711 = hx711.read(0)
+	  	elseif ( gpio.read(2) == 1 and gpio.read(3) == 0 ) then
+	  		print("Poucas garrafas no triturador.")
+	  		gpio.write(4, gpio.LOW)
+	  		gpio.write(1, gpio.HIGH)
 
-      if topico == "vidro" then
-        print("Topico: ", topico)
-        print("Garrafa de vidro entrou na Máquina! peso: ", conteudo)
+	  	elseif ( gpio.read(2) == 1 and gpio.read(3) == 1 ) then
+	  		print("Muitas garrafas no triturador.")
+	  		gpio.write(1, gpio.HIGH)
 
-        dados = {}
-
-        if read_hx711 == peso then
-          print('Peso confere!')
-          dados[0] = nil
-        else
-          print('Peso não confere')
-          dados[0] = 'peso'
-        end  
-
-        if sensor_capacitivo_vidro == 1 then
-          print('Sensor capacitivo verificou que é vidro')
-          dados[1] = nil
-        else
-          print('Sensor capacitivo para o vidro não acionou')
-          dados[1] = 'VidroF'
-
-        end 
-
-        if sensor_capacitivo_plastico == 0 then
-          print('Sensor capacitivo verificou que não é plástico')
-          dados[2] = nil     
-        else
-          print('Sensor capacitivo de plástico acionou')
-          dados[2] ='PlasticoV'
-
-        end
-
-        if sensor_capacitivo_verificar == 0  then
-          print('Sensor capacitivo de verificação não acionou')
-          dados[3] = nil
-        else
-          print('Sensor capacitivo de verificação acionou')
-          dados[3] ='VerificarV'
-        end
-
-        if dados ~= nil then
-          topico_base_publicacao = topico_base_publicacao .. '/vidro'
-          cliente_mqtt.mqtt_dados(topico_base_publicacao, topico_receber, dados);
-
-        else
-          motor.rotacionar_motor("esquerda") 
-        end
-
-
-      elseif topico == "plastico" then
-        print("Topico: ", topico)
-        print("Garrafa de plástico entrou na Máquina! peso: ", conteudo)
-
-      end
+	  	end
 
     end)
     timer_principal:start()
@@ -101,16 +55,8 @@ end
   
 function start()
   print(node.bootreason())
-
   print('\n\n=== Rodando');
   
-  wlan = WLan("Matheus", "batatafrita123", IP, NETMASK, GW)
-  wlan.status_wifi();
-  motor = MotorPasso(8,7,6,5)
-
-  cliente_mqtt = ClienteMqtt("192.168.0.13", 1883, 'id') --função MQTT
-
-
   modo_operacao = MODOS_OPERACAO.NORMAL
   local timer_principal = tmr.create()
   loop_principal(timer_principal);
